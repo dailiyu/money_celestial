d8b8f1d165c1fef7e1dfef4b1f40b6897bee2df4
Chen-Wanying <417307918@qq.com>
2024-09-13 15:28:27 +0800

    1

---
+++ components/shopList/shopList.vue
@@ -0,0 +1,90 @@
+<template>
+	<view>
+		<view class="shop_list">
+			<view class="shop_item flex" @click="toDetail">
+				<image src="" mode="aspectFill" class="shop_pic"></image>
+				<view class="" style="flex: 1;">
+					<view class="shop_name">
+						味多美家庭烘焙
+					</view>
+					<view class="pic_box flex">
+						<image src="@/static/star.png" mode="widthFix" class="star_pic"></image>
+						<view class="point">
+							4.9
+						</view>
+					</view>
+					<view class="flex">
+						<image src="@/static/locate_orange.png" mode="widthFix" class="location_pic"></image>
+						<view class="district">
+							南海区
+						</view>
+					</view>
+				</view>
+				<view class="distance">
+					1.51km
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"shopList",
+		data() {
+			return {
+				
+			};
+		},
+		methods(){
+			function toDetail(){
+				uni.navigateTo({
+					url: '/pages/merchant/merchant_detail'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.shop_list {
+	.shop_item {
+		padding: 20rpx 44rpx 20rpx 20rpx;
+		background-color: #fff;
+		align-items: flex-end;
+		.shop_pic {
+			width: 114rpx;
+			height: 114rpx;
+			margin-right: 20rpx;
+			background-color: #ccc;
+			border-radius: 6px;
+		}
+		.shop_name {
+			font-size: 26rpx;
+		}
+		.pic_box {
+			margin: 10rpx 0 20rpx;
+			.star_pic {
+				width: 22rpx;
+			}
+			.point {
+				font-size: 20rpx;
+				color: #FC5908;
+				margin-left: 20rpx;
+			}
+		}
+		.location_pic {
+			width: 22rpx;
+			margin-right: 20rpx;
+		}
+		.district {
+			font-size: 20rpx;
+			color: #1B46CC;
+		}
+		.distance {
+			font-size: 20rpx;
+			color: #FC5908;
+		}
+	}
+}
+</style>
---
+++ pages/agent/merchant_list.vue
@@ -0,0 +1,73 @@
+<template>
+	<view>
+		<navBar title="商家列表"></navBar>
+		<view class="filter_list">
+			<view class="flex_center" @click="getType">
+				<image src="@/static/category.png" mode="widthFix" class="type_pic"></image>
+				<view>
+					类目
+				</view>
+			</view>
+			<view class="flex_center">
+				<view class="">
+					入驻时间
+				</view>
+				<view class="" @click="distance='down'" v-if="distance=='up'">
+					<image src="@/static/arrow-active.png" mode="widthFix" class="arrow_fill"></image>
+					<image src="@/static/arrow-inactive.png" mode="widthFix" class="arrow_fill"></image>
+				</view>
+				<view class="" @click="distance='up'" v-if="distance=='down'">
+					<image src="@/static/arrow-inactive.png" mode="widthFix" class="arrow_fill" style="transform: rotate(180deg);"></image>
+					<image src="@/static/arrow-active.png" mode="widthFix" class="arrow_fill" style="transform: rotate(180deg);"></image>
+				</view>
+			</view>
+		</view>
+		<view class="content">
+			<shopList></shopList>
+		</view>
+	</view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const getType = ()=>{
+	uni.showActionSheet({
+		itemList: ['美食', '服饰'],
+		success(res) {
+			console.log(res.tapIndex)
+		}
+	})
+}
+const toSettle = ()=>{
+	uni.navigateTo({
+		url: '/pages/merchant/merchant_intro'
+	})
+}
+
+const distance = ref('up')
+</script>
+
+<style lang="scss" scoped>
+.filter_list {
+	font-size: 30rpx;
+	padding: 35rpx;
+	background-color: #fff;
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+	.type_pic {
+		width: 26rpx;
+		margin-right: 20rpx;
+	}
+	.arrow_fill {
+		width: 20rpx;
+		height: 10rpx;
+		display: block;
+		margin-left: 10rpx;
+		&:last-child {
+			margin-top: 6rpx;
+		}
+	}
+}
+
+</style>
