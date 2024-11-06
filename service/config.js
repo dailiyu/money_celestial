a1d27dc15ac4a0b8040ec428a3dee930275af856
Chen-Wanying <417307918@qq.com>
2024-11-06 11:54:10 +0800

    修改首页数据获取逻辑

--- service/config.js
+++ service/config.js
@@ -1,7 +1,7 @@
 // export  const uploadUrl="https://api.maxcang.com/api/upload/image/"
-export  const uploadUrl='http://192.168.110.46:8000/api/upload/image/'
+// export  const uploadUrl='http://192.168.110.46:8000/api/upload/image/'
 
-// export  const uploadUrl='http://192.168.110.47:8000/api/upload/image/'
+export  const uploadUrl='http://192.168.110.47:8000/api/upload/image/'
  
 
 
